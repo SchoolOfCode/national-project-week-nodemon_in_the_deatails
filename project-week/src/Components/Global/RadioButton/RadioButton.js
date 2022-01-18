03450dbc35f6c35ddc87a982b3e5d62a6e3a7936
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-01-18 12:34:11 +0000

    WIP: Add functionality to the Radio Buttons components

--- project-week/src/Components/Global/RadioButton/RadioButton.js
+++ project-week/src/Components/Global/RadioButton/RadioButton.js
@@ -1,13 +1,11 @@
-import React from 'react'
-
-export default function RadioButton() {
-    return (
-        <div>
-            <input type="radio" value="1" name="mood" /> 1
-            <input type="radio" value="2" name="mood" /> 2
-            <input type="radio" value="3" name="mood" /> 3
-            <input type="radio" value="4" name="mood" /> 4
-            <input type="radio" value="5" name="mood" /> 5
-        </div>
-    )
+export default function RadioButton({ handleClick }) {
+  return (
+    <div onChange={handleClick}>
+      <input type="radio" value="1" name="mood" /> 1
+      <input type="radio" value="2" name="mood" /> 2
+      <input type="radio" value="3" name="mood" /> 3
+      <input type="radio" value="4" name="mood" /> 4
+      <input type="radio" value="5" name="mood" /> 5
+    </div>
+  );
 }
--- project-week/src/Components/Post/Post.js
+++ project-week/src/Components/Post/Post.js
@@ -4,20 +4,34 @@ import InputField from "../Global/InputField/InputField";
 import { useState } from "react";
 
 export default function Post() {
-
   const [posts, setPosts] = useState([]);
 
-  function addPost(post){
-    setPosts([post, ...posts])
-    console.log(posts)
+  function addPost(post) {
+    setPosts([post, ...posts]);
+    console.log(posts);
+  }
+
+  const [mood, setMood] = useState("");
+
+  function getMoodValue(e) {
+    setMood(e.target.value);
+    console.log(mood);
   }
 
   return (
     <div>
       <InputField />
-      <RadioButton />
+      <RadioButton
+        handleClick={function () {
+          getMoodValue(mood);
+        }}
+      />
       <InputField />
-      <Button handleClick={function(){addPost(posts)}}/>
+      <Button
+        handleClick={function () {
+          addPost(posts);
+        }}
+      />
     </div>
   );
 }
