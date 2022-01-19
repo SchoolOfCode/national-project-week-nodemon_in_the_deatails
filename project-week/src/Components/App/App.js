d70658721021a3a64138f03461f2ab3066192a8e
fonox1 <abdih0698@gmail.com>
2022-01-19 16:52:44 +0000

    UI/add-css-creatpost update

--- project-week/src/Components/App/App.js
+++ project-week/src/Components/App/App.js
@@ -4,13 +4,16 @@ import Dashboard from "../Dashboard/Dashboard";
 import CreatePost from "../CreatePost/CreatePost";
 function App() {
   return (
-    <div className="App">
+
+      <div className="container">
+         <div className="App">
       <header className="App-header">
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<CreatePost />} />
         </Routes>
       </header>
+      </div>
     </div>
   );
 }
